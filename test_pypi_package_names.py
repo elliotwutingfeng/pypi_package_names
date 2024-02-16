9e8bee51d7769f7ddadabb84ddd7b22a81c9aeda
Wu Tingfeng <wutingfeng@outlook.com>
2024-02-16 13:30:20 +0800

    Check if str or unicode type

--- test_pypi_package_names.py
+++ test_pypi_package_names.py
@@ -1,14 +1,29 @@
 import json
+import sys
 import unittest
+
+try:
+    from typing import Any  # Python 3.5 or later required
+except ImportError:
+    pass
 
 from pypi_package_names import get_unique_package_names
 
 
 class TestGetUniquePackageNames(unittest.TestCase):
     @staticmethod
+    def _is_str_or_unicode(s):  # type: (Any) -> bool
+        if isinstance(s, str):
+            return True
+        is_python2 = sys.version_info[0] == 2
+        return is_python2 and type(s).__name__ == "unicode"
+
+    @staticmethod
     def _check_package_names(package_names):  # type: (list[str]) -> None
         assert isinstance(package_names, list)
-        assert all(isinstance(s, str) for s in package_names)
+        assert all(
+            TestGetUniquePackageNames._is_str_or_unicode(s) for s in package_names
+        )
         assert "black" in package_names
         assert "requests" in package_names
 
